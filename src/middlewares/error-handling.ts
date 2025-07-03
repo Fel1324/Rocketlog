6e291ff1e35126bdeedc17464545e376fdcc1daa
Rafael Roberto de Oliveira <rafael.roberto200618@gmail.com>
2025-07-03 14:42:07 -0300

    fix: @types/express compatibility

--- src/middlewares/error-handling.ts
+++ src/middlewares/error-handling.ts
@@ -2,7 +2,7 @@ import { Request, Response, NextFunction } from "express"
 import { AppError } from "@/utils/app-error"
 import { ZodError } from "zod"
 
-export function errorHandling(error: any, request: Request, response: Response, next: NextFunction){
+export function errorHandling(error: any, request: Request, response: Response, next: NextFunction) {
   if(error instanceof AppError){
     return response.status(error.statusCode).json({ message: error.message })
   }
